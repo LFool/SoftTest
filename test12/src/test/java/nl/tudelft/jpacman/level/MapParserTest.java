30ab5cb1f60f221400de9f9ad9246880ebc2620e
LFool <1712199884@qq.com>
2020-05-31 21:23:46 +0800

    modify test12

--- test12/src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ test12/src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -6,141 +6,74 @@ import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.npc.Ghost;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 class MapParserTest {
 
-    private final LevelFactory levelCreator = Mockito.mock(LevelFactory.class);
-    private final BoardFactory boardCreator = Mockito.mock(BoardFactory.class);
-    private final Square wall = Mockito.mock(Square.class);
-    private final Square ground = Mockito.mock(Square.class);
-    private final Square palletSquare = Mockito.mock(Square.class);
-    private final Square playerSquare = Mockito.mock(Square.class);
-    private final Square ghostSquare = Mockito.mock(Square.class);
-    private final Pellet pellet = Mockito.mock(Pellet.class);
-    private final Ghost ghost = Mockito.mock(Ghost.class);
-    private final List<String> string = null;
-
     private MapParser mapParser;
-    /**
-     * create a before each.
-     */
+    private final LevelFactory levelFactory = mock(LevelFactory.class);
+    private final BoardFactory boardFactory = mock(BoardFactory.class);
+
     @BeforeEach
     void setup() {
-        mapParser = new MapParser(levelCreator, boardCreator);
-        Mockito.when(boardCreator.createGround()).thenReturn(ground);
-        Mockito.when(boardCreator.createWall()).thenReturn(wall);
-        Mockito.when(levelCreator.createPellet()).thenReturn(pellet);
-        Mockito.when(boardCreator.createGround()).thenReturn(playerSquare);
-        Mockito.when(boardCreator.createGround()).thenReturn(ghostSquare);
-        Mockito.when(boardCreator.createGround()).thenReturn(palletSquare);
-        Mockito.when(levelCreator.createGhost()).thenReturn(ghost);
+        mapParser = new MapParser(levelFactory, boardFactory);
+
+        when(boardFactory.createGround()).thenReturn(mock(Square.class));
+        when(boardFactory.createWall()).thenReturn(mock(Square.class));
+
+        when(levelFactory.createGhost()).thenReturn(mock(Ghost.class));
+        when(levelFactory.createPellet()).thenReturn(mock(Pellet.class));
     }
-
-
-    /**
-     *Nice Weather test for checking ground is created when empty space is passed.
-     */
-    @Test
-    void forGround() {
-        mapParser.parseMap(Lists.newArrayList(" "));
-        Mockito.verify(boardCreator).createGround();
-    }
-
-    /**
-     * Nice weather test for checking if wall is created when "#" is passed.
-     */
 
     @Test
-    void forWall() {
-        mapParser.parseMap(Lists.newArrayList("#"));
-        Mockito.verify(boardCreator).createWall();
+    @Order(1)
+    @DisplayName("null 文件名")
+    void nullFile() {
+        assertThatThrownBy(() -> {
+            mapParser.parseMap((String) null);
+        }).isInstanceOf(NullPointerException.class);
     }
-
-    /**
-     * Nice weather test for creating a pallet in the square when "." is passed.
-     */
 
     @Test
-    void forPallet() {
-        mapParser.parseMap(Lists.newArrayList("."));
-        Mockito.verify(boardCreator).createGround();
-        Mockito.verify(levelCreator).createPellet();
-        Mockito.verify(pellet).occupy(palletSquare);
+    @Order(2)
+    @DisplayName("文件不存在")
+    void notExistFile() {
+        String file = "/notexistmap.txt";
+        assertThatThrownBy(() -> {
+            mapParser.parseMap(file);
+        }).isInstanceOf(PacmanConfigurationException.class)
+            .hasMessage("Could not get resource for: " + file);
     }
-
-    /**
-     * Nice weather test for creating a player.
-     */
 
     @Test
-    void forPlayer() {
-        mapParser.parseMap(Lists.newArrayList("P"));
-        Mockito.verify(boardCreator).createGround();
-        Mockito.verify(levelCreator).createLevel(Mockito.any(),
-            Mockito.anyList(), Mockito.anyList());
+    @Order(3)
+    @DisplayName("存在文件")
+    void exitFile() throws IOException {
+        String file = "/simplemap.txt";
+        mapParser.parseMap(file);
+
+        verify(boardFactory, times(4)).createGround();
+        verify(boardFactory, times(2)).createWall();
+        verify(levelFactory).createGhost();
     }
 
-    /**
-     * Bad weather Test for mapParser with null input.
-     */
     @Test
-    void noInput() {
-        try {
-            mapParser.parseMap(string);
-        }
-        catch (PacmanConfigurationException e) {
-            assertThat(e.getMessage()).contains("Input text cannot be null.");
-        }
+    @Order(4)
+    @DisplayName("不可识别的地图文件")
+    void unrecognizedMap() {
+        String file = "/unrecognizedmap.txt";
+        assertThatThrownBy(() -> {
+            mapParser.parseMap(file);
+        }).isInstanceOf(PacmanConfigurationException.class)
+            .hasMessage("Invalid character at 0,0: A");
     }
-
-
-
-    /**
-     * Bad weather test for MapParser should ensure equal width.
-     */
-    @Test
-    void equalWidth() {
-        try {
-            mapParser.parseMap(Lists.newArrayList("##", "   "));
-        }
-        catch (PacmanConfigurationException e){
-            assertThat(e.getMessage()).contains("Input text lines are not of equal width.");
-        }
-    }
-
-
-    /**
-     * Bad Weather Test for empty input
-     */
-    @Test
-    void errorEmptyList() {
-        try {
-            mapParser.parseMap(Lists.newArrayList());
-        }
-        catch (PacmanConfigurationException e) {
-            assertThat(e.getMessage()).contains("Input text must consist of at least 1 row.");
-
-        }
-    }
-
-
-    /**
-     * Bad weather Test for mapParser with 0 width.
-     */
-    @Test
-    void zeroWidth() {
-        try {
-            mapParser.parseMap(Lists.newArrayList(""));
-        }
-        catch (PacmanConfigurationException e) {
-            assertThat(e.getMessage()).contains("Input text lines cannot be empty.");
-        }
-    }
-
 }
